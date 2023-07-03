5d9362da04ee20df85082f7e0313de7db1ffdf42
atillaturkmen <turkmenatilla522@gmail.com>
2023-07-03 16:57:02 +0300

    added circle mode

--- js/selectors/ToolSelector.js
+++ js/selectors/ToolSelector.js
@@ -1,6 +1,7 @@
 import {Pen} from "../drawingModes/Pen.js";
 import {Path} from "../drawingModes/Path.js";
 import {Rectangle} from "../drawingModes/Rectangle.js";
+import {Circle} from "../drawingModes/Circle.js";
 
 export class ToolSelector {
     constructor(context, canvas) {
@@ -10,11 +11,13 @@ export class ToolSelector {
         this.pen = new Pen(context, canvas);
         this.path = new Path(context, canvas);
         this.rect = new Rectangle(context, canvas);
+        this.circle = new Circle(context, canvas);
 
         // Bind the functions to their respective objects
         this.penStartDraw = this.pen.startDraw.bind(this.pen);
-        this.pathStartPath = this.path.startPath.bind(this.path);
-        this.rectStartRect = this.rect.startRect.bind(this.rect);
+        this.pathStart = this.path.startPath.bind(this.path);
+        this.rectStart = this.rect.startRect.bind(this.rect);
+        this.circleStart = this.circle.startCircle.bind(this.circle);
     }
 
     setMode(e, mode) {
@@ -27,13 +30,18 @@ export class ToolSelector {
                 break;
 
             case 'path':
-                this.canvas.addEventListener("mousedown", this.pathStartPath);
-                this.activeEvent = this.pathStartPath;
+                this.canvas.addEventListener("mousedown", this.pathStart);
+                this.activeEvent = this.pathStart;
                 break;
 
             case 'rect':
-                this.canvas.addEventListener("mousedown", this.rectStartRect);
-                this.activeEvent = this.rectStartRect;
+                this.canvas.addEventListener("mousedown", this.rectStart);
+                this.activeEvent = this.rectStart;
+                break;
+
+            case 'circle':
+                this.canvas.addEventListener("mousedown", this.circleStart);
+                this.activeEvent = this.circleStart;
                 break;
 
             default:
