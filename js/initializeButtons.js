9aba6ead8b384a2b449770a93c6835ba6a350b94
atillaturkmen <turkmenatilla522@gmail.com>
2023-07-03 15:48:35 +0300

    refactor

---
+++ js/initializeButtons.js
@@ -0,0 +1,45 @@
+import {ColorSelector} from "./ColorSelector.js";
+import {ToolSelector} from "./ToolSelector.js";
+import {SizeSelector} from "./SizeSelector.js";
+
+export function initializeButtons(context, canvas) {
+    // Color Selection Initialization
+
+    const colorSelector = new ColorSelector(context);
+    const colorButtons = document.getElementById('colors').children;
+
+    for (const colorButton of colorButtons) {
+        colorButton.addEventListener('click', (e) => {
+            colorSelector.setColor(e, colorButton.classList.value.replace(/bg-(\w*).*/, '$1'));
+        });
+    }
+
+    // Tool Selection Initialization
+
+    const toolSelector = new ToolSelector(context, canvas);
+    const tools = document.getElementsByClassName('tool');
+
+    for (const tool of tools) {
+        tool.addEventListener('click', (e) => {
+            toolSelector.setMode(e, tool.id);
+        });
+    }
+
+    // Size Selection Initialization
+
+    const sizeSelector = new SizeSelector(context);
+    const sizeButtons = document.getElementsByClassName('size');
+    for (const sizeButton of sizeButtons) {
+        sizeButton.addEventListener('click', (e) => {
+            sizeSelector.setSize(e, sizeButton.id);
+        });
+    }
+
+    // Clear Canvas Button Initialization
+
+    function clearCanvas() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    document.getElementById('clear').addEventListener('click', clearCanvas);
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import '../css/style.css'
-import {ColorSelector} from "./ColorSelector.js";
-import {ToolSelector} from "./ToolSelector.js";
-import {SizeSelector} from "./SizeSelector.js";
+import {initializeButtons} from "./initializeButtons.js";
 
 const canvas = document.getElementById("canvas");
 const width = 1920;
@@ -14,43 +12,7 @@ const context = canvas.getContext("2d");
 canvas.height = height;
 canvas.width = width;
 
-// Color Selection Initialization
-
-const colorSelector = new ColorSelector(context);
-const colorButtons = document.getElementById('colors').children;
-
-for (const colorButton of colorButtons) {
-    colorButton.addEventListener('click', (e) => {
-        colorSelector.setColor(e, colorButton.classList.value.replace(/bg-(\w*).*/, '$1'));
-    });
-}
-
-// Tool Selection Initialization
-
-const toolSelector = new ToolSelector(context, canvas);
-const tools = document.getElementsByClassName('tool');
-
-for (const tool of tools) {
-    tool.addEventListener('click', (e) => {
-        toolSelector.setMode(e, tool.id);
-    });
-}
-
-// Size Selection Initialization
-
-const sizeSelector = new SizeSelector(context);
-const sizeButtons = document.getElementsByClassName('size');
-for (const sizeButton of sizeButtons) {
-    sizeButton.addEventListener('click', (e) => {
-        sizeSelector.setSize(e, sizeButton.id);
-    });
-}
-
-// Clear canvas
-function clearCanvas() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-}
-document.getElementById('clear').addEventListener('click', clearCanvas);
+initializeButtons(context, canvas);
 
 // set default settings
 context.lineCap = 'round';
