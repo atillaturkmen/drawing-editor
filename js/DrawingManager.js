42f642929a365334e472b38b68283b599fdc9d9e
atillaturkmen <turkmenatilla522@gmail.com>
2023-07-04 16:08:58 +0300

    clear undone history after clear

--- js/DrawingManager.js
+++ js/DrawingManager.js
@@ -12,6 +12,7 @@ export class DrawingManager {
 
     static clearHistory() {
         DrawingManager.history = [];
+        DrawingManager.undoneHistory = [];
     }
 
     static undoLastShape() {
